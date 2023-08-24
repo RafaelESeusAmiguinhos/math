302dd99e10232c58047d7860d7f92af6fd11bd47
RafaelESeusAmiguinhos <114957968+RafaelESeusAmiguinhos@users.noreply.github.com>
2023-08-24 16:51:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var name1 = "Rafael";
+var name2 = "draghi";
+var junto =  name1.concat(name2);
+console.log (junto);
+
+var numero1 = 6 ;
+var numero2 = 7 ;
+var numeroJ = numero1 + numero2;
+console.log (numeroJ)
